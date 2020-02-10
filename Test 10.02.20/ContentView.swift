26b12ff5bc024d12e96148db7657afda1230a7bd
samatumb <39953880+samatumb@users.noreply.github.com>
2020-02-11 00:16:42 +0300

    Initial Commit

---
+++ Test 10.02.20/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test 10.02.20
+//
+//  Created by Samat Umirbekov on 11.02.2020.
+//  Copyright © 2020 Samat Umirbekov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
